f4d31a33423650b2d76885ad09fa88293157d2bb
Romitou <romitou@protonmail.com>
2022-05-16 18:52:43 +0200

    :bug: Oops, bad binary name

--- build.bat
+++ build.bat
@@ -1,4 +1,4 @@
-cmd /C "set GOOS=linux&& set GOARCH=arm&& go build -o builds/panoramix-linux-arm"
-git update-index --chmod=+x builds/panoramix-linux-arm
-cmd /C "set GOOS=windows&& set GOARCH=amd64&& go build -o builds/panoramix-win-amd64.exe"
-git update-index --chmod=+x builds/panoramix-win-amd64.exe
+cmd /C "set GOOS=linux&& set GOARCH=arm&& go build -o builds/gostats-linux-arm"
+git update-index --chmod=+x builds/gostats-linux-arm
+cmd /C "set GOOS=windows&& set GOARCH=amd64&& go build -o builds/gostats-win-amd64.exe"
+git update-index --chmod=+x builds/gostats-win-amd64.exe
